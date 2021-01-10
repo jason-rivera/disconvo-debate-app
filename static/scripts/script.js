16351837f663ad1e273f4cc222a6f8af432e38cb
tz18 <ramonrakow@gmail.com>
2021-01-09 18:01:48 -0800

    Use socket.io example chat

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,10 +1,20 @@
-var socket = io()
-const messageForm = document.getElementById('send-container')
+var socket = io();
 
-socket.on('chat-message', data =>{
-    console.log(data)
-})
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var input = document.getElementById('input');
 
-messageForm.addEventListener('submit', e =>{
-    e.preventDefault()
-})
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	if (input.value) {
+	  socket.emit('chat message', input.value);
+	  input.value = '';
+		}
+});
+
+	socket.on('chat message', function(msg) {
+	var item = document.createElement('li');
+	item.textContent = msg;
+	messages.appendChild(item);
+	window.scrollTo(0, document.body.scrollHeight);
+});
