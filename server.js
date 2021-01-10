0d9f66776c767fd48bf0ad3636a4c37df5659e9f
tz18 <ramonrakow@gmail.com>
2021-01-09 19:12:27 -0800

    move stuff around, fix swiping with hammerjs

--- server.js
+++ server.js
@@ -5,30 +5,6 @@ const io = require('socket.io')(http);
 
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/conversation', (req, res) => {
-  res.sendFile(__dirname + '/conversation.html');
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/registration.html')
-});
-
-app.get('/profile', (req, res) => {
-    res.sendFile(__dirname + '/profile.html')
-});
-
-app.get('/rules', (req, res) => {
-    res.sendFile(__dirname + '/rules.html')
-});
-
-app.get('/dislikes', (req, res) => {
-    res.sendFile(__dirname + '/dislikes.html')
-})
-
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('disconnect', () => {
