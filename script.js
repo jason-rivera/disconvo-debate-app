b27e806a428804cfb0141a9813638029ce0406cc
Ferrel Anthoni <ferrelanthoni@gmail.com>
2021-01-09 17:00:39 -0800

    fixed CORS error in the browser console

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const socket = io('http://localhost:3000')
+const messageForm = document.getElementById('send-container')
 
 socket.on('chat-message', data =>{
     console.log(data)
+})
+
+messageForm.addEventListener('submit', e =>{
+    e.preventDefault()
 })
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const io = require('socket.io')(3000);
+const io = require("socket.io")(3000, {
+    cors:{
+        origin:"*",
+    }
+});
 
 io.on('connection', socket => {
     console.log('New User')
