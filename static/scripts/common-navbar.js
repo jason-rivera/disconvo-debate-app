398505a44184868cb44a3d70cc65aaaf4ddf4a0a
Jason Rivera <jp_rivera_@hotmail.com>
2021-01-10 11:03:24 -0800

    added items to navbar, edited profile colour

--- static/scripts/common-navbar.js
+++ static/scripts/common-navbar.js
@@ -2,7 +2,16 @@ document.getElementById('foot').innerHTML = `
   <nav class="navbar fixed-bottom navbar bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="profile.html"><i class="fas fa-user-circle"></i></a>
+
+    <a class="navbar-brand" href="matching.html"><i class="fas fa-search"></i></a>
+    
     <a class="navbar-brand" href="index.html"><i class="fas fa-home"></i></a>
+
+
+
+    
+    <a class="navbar-brand" href="new-card.html"><i class="fas fa-plus-square"></i></a>
+    
     <a class="navbar-brand" href="rules.html"><i class="fas fa-cog"></i></a>
   </div>
   </nav>
