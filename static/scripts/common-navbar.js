c3aa5f52ed4ad119888a1005bd8a0c7965828046
Jason Rivera <jp_rivera_@hotmail.com>
2021-01-10 10:49:21 -0800

    increased size of sub-headers

--- static/scripts/common-navbar.js
+++ static/scripts/common-navbar.js
@@ -2,7 +2,7 @@ document.getElementById('foot').innerHTML = `
   <nav class="navbar fixed-bottom navbar bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="profile.html"><i class="fas fa-user-circle"></i></a>
-    <a class="navbar-brand" href="matching.html"><i class="fas fa-home"></i></a>
+    <a class="navbar-brand" href="index.html"><i class="fas fa-home"></i></a>
     <a class="navbar-brand" href="rules.html"><i class="fas fa-cog"></i></a>
   </div>
   </nav>
